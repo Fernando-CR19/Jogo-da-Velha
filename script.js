9ae4327b988ca7712f206da81f3fedfbf24e2619
Fernando_CR19 <fernandocr019@gmail.com>
2022-06-21 22:45:28 -0300

    Retira contador de vitórias devido a um BUG

--- script.js
+++ script.js
@@ -2,17 +2,10 @@ const player1 = 'X'
 const player2 = 'O'
 let currentPlayer = player1
 let fimDoJogo = false
-let numX = 0
-let numO = 0
 
-const btn = document.querySelector("#resetarJogo")
+const btn = document.querySelector("#btnRefresh")
 const jogo = document.getElementsByClassName('espaco')
 const mensagem = document.getElementsByClassName('winner')[0]
-const ptsX = document.getElementById('ptsX')
-const ptsO = document.getElementById('ptsO')
-
-ptsX.innerHTML = `X - ${numX}`
-ptsO.innerHTML = `O - ${numO}`
 
 function jogar() {
     for (let i = 0; i < jogo.length; i++) {
@@ -42,14 +35,20 @@ function jogar() {
 
 jogar()
 
-for (let i = 0; i < jogo.length; i++) {
-    const empate = jogo[i];
-    function chegarEmpate() {
-        if (empate.innerHTML !== '') {
-            mensagem.innerHTML = 'Empate'
-        }
+function chegarEmpate() {
+    if (jogo[0].innerHTML !== '' &&
+        jogo[1].innerHTML !== '' &&
+        jogo[2].innerHTML !== '' &&
+        jogo[3].innerHTML !== '' &&
+        jogo[4].innerHTML !== '' &&
+        jogo[5].innerHTML !== '' &&
+        jogo[6].innerHTML !== '' &&
+        jogo[7].innerHTML !== '' &&
+        jogo[8].innerHTML !== '') {
+        mensagem.innerHTML = 'Empate'
     }
 }
+
 
 function checarVitoria() {
     if (jogo[0].innerHTML === currentPlayer && jogo[1].innerHTML === currentPlayer && jogo[2].innerHTML === currentPlayer) {
@@ -94,25 +93,6 @@ function mudarJogador() {
     }
 }
 
-for (let i = 0; i < jogo.length; i++) {
-    const reset = jogo[i];
-    btn.addEventListener("click", function () {
-        reset.innerHTML = ''
-        mensagem.innerHTML = ''
-        fimDoJogo = false
-    })
-}
-
-function pontos() {
-    if (mensagem.innerHTML === 'X venceu!') {
-        numX = numX + 1
-        ptsX.innerHTML = `X - ${numX}`
-        mudarJogador()
-    }
-
-    if (mensagem.innerHTML === 'O venceu!') {
-        numO = numO + 1
-        ptsO.innerHTML = `O - ${numO}`
-        mudarJogador()
-    }
-}
+btn.addEventListener("click", function () {
+    location.reload()
+})
